26b543ce29a8118ee56120827f03853b1429f589
Sasha Razumov <sasheazy@gmail.com>
2021-01-13 23:52:20 +1000

    Delete js directory

--- js/set-content-height.js
+++
@@ -1,7 +0,0 @@
-function setHeight () {
-  document.querySelector('.content').style.height = window.innerWidth <= 768
-    ? window.innerHeight + 'px'
-    : 'auto'
-}
-setHeight()
-window.addEventListener('resize', setHeight)
